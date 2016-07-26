d1cda674e4ccfd771553a4a763f24eb5ea652d4b
eugenelet <eugeneleeeutzuan@gmail.com>
2016-07-26 23:43:30 +0800

    sharing of adders for data_num and accu_element

--- 01_RTL/kMeans.v
+++ 01_RTL/kMeans.v
@@ -599,13 +599,52 @@ always @(posedge clk) begin
     min_idx <= group_acc_clus_count;
 end
 
-
 //DATA_NUM (CAN SHARE SAME ADDER)
+
+wire      [11:0]  data_num_wire0;
+wire      [11:0]  data_num_wire1;
+wire      [11:0]  data_num_wire2;
+wire      [11:0]  data_num_wire3;
+always @(*) begin
+  case(min_idx)
+    'd0: begin
+      data_num_wire0 = data_num0 + 'd1;
+      data_num_wire1 = data_num1;
+      data_num_wire2 = data_num2;
+      data_num_wire3 = data_num3;
+    end
+    'd1: begin
+      data_num_wire0 = data_num0;
+      data_num_wire1 = data_num1 + 'd1;
+      data_num_wire2 = data_num2;
+      data_num_wire3 = data_num3;
+    end
+    'd2: begin
+      data_num_wire0 = data_num0;
+      data_num_wire1 = data_num1;
+      data_num_wire2 = data_num2 + 'd1;
+      data_num_wire3 = data_num3;
+    end
+    'd3: begin
+      data_num_wire0 = data_num0;
+      data_num_wire1 = data_num1;
+      data_num_wire2 = data_num2;
+      data_num_wire3 = data_num3 + 'd1;
+    end
+    default: begin
+      data_num_wire0 = data_num0;
+      data_num_wire1 = data_num1;
+      data_num_wire2 = data_num2;
+      data_num_wire3 = data_num3;
+    end
+  endcase
+end
+
 always @(posedge clk) begin
   if (!rst_n)
     data_num0 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d0) 
-    data_num0 <= data_num0 + 'd1;    
+    data_num0 <= data_num_wire0;    
   else if (current_state == ST_CHECK)
     data_num0 <= 'd0;
 end
@@ -613,7 +652,7 @@ always @(posedge clk) begin
   if (!rst_n)
     data_num1 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d1) 
-    data_num1 <= data_num1 + 'd1;    
+    data_num1 <= data_num_wire1;    
   else if (current_state == ST_CHECK)
     data_num1 <= 'd0;
 end
@@ -621,7 +660,7 @@ always @(posedge clk) begin
   if (!rst_n)
     data_num2 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d2) 
-    data_num2 <= data_num2 + 'd1;    
+    data_num2 <= data_num_wire2;    
   else if (current_state == ST_CHECK)
     data_num2 <= 'd0;
 end
@@ -629,18 +668,83 @@ always @(posedge clk) begin
   if (!rst_n)
     data_num3 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d3) 
-    data_num3 <= data_num3 + 'd1;    
+    data_num3 <= data_num_wire3;    
   else if (current_state == ST_CHECK)
     data_num3 <= 'd0;
 end
 
 
 //ACCU ELEMENT
+
+wire      [19:0]  accu_element_wirex0;
+wire      [19:0]  accu_element_wirex1;
+wire      [19:0]  accu_element_wirex2;
+wire      [19:0]  accu_element_wirex3;
+wire      [19:0]  accu_element_wirey0;
+wire      [19:0]  accu_element_wirey1;
+wire      [19:0]  accu_element_wirey2;
+wire      [19:0]  accu_element_wirey3;
+always @(*) begin
+  case(min_idx)
+    'd0: begin
+      accu_element_wirex0 = accu_element_x0 + total_element[15:8];
+      accu_element_wirex1 = accu_element_x1;
+      accu_element_wirex2 = accu_element_x2;
+      accu_element_wirex3 = accu_element_x3;
+      accu_element_wirey0 = accu_element_y0 + total_element[7:0];
+      accu_element_wirey1 = accu_element_y1;
+      accu_element_wirey2 = accu_element_y2;
+      accu_element_wirey3 = accu_element_y3;
+    end
+    'd1: begin
+      accu_element_wirex0 = accu_element_x0;
+      accu_element_wirex1 = accu_element_x1 + total_element[15:8];
+      accu_element_wirex2 = accu_element_x2;
+      accu_element_wirex3 = accu_element_x3;
+      accu_element_wirey0 = accu_element_y0;
+      accu_element_wirey1 = accu_element_y1 + total_element[7:0];
+      accu_element_wirey2 = accu_element_y2;
+      accu_element_wirey3 = accu_element_y3;
+    end
+    'd2: begin
+      accu_element_wirex0 = accu_element_x0;
+      accu_element_wirex1 = accu_element_x1;
+      accu_element_wirex2 = accu_element_x2 + total_element[15:8];
+      accu_element_wirex3 = accu_element_x3;
+      accu_element_wirey0 = accu_element_y0;
+      accu_element_wirey1 = accu_element_y1;
+      accu_element_wirey2 = accu_element_y2 + total_element[7:0];
+      accu_element_wirey3 = accu_element_y3;
+    end
+    'd3: begin
+      accu_element_wirex0 = accu_element_x0;
+      accu_element_wirex1 = accu_element_x1;
+      accu_element_wirex2 = accu_element_x2;
+      accu_element_wirex3 = accu_element_x3 + total_element[15:8];
+      accu_element_wirey0 = accu_element_y0;
+      accu_element_wirey1 = accu_element_y1;
+      accu_element_wirey2 = accu_element_y2;
+      accu_element_wirey3 = accu_element_y3 + total_element[7:0];
+    end
+    default: begin
+      accu_element_wirex0 = accu_element_x0;
+      accu_element_wirex1 = accu_element_x1;
+      accu_element_wirex2 = accu_element_x2;
+      accu_element_wirex3 = accu_element_x3;
+      accu_element_wirey0 = accu_element_y0;
+      accu_element_wirey1 = accu_element_y1;
+      accu_element_wirey2 = accu_element_y2;
+      accu_element_wirey3 = accu_element_y3;
+    end
+  endcase
+end
+
+
 always @(posedge clk) begin
   if (!rst_n)
     accu_element_x0 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d0) 
-    accu_element_x0 <= accu_element_x0 + total_element[15:8];    
+    accu_element_x0 <= accu_element_wirex0;    
   else if (current_state == ST_CHECK)
     accu_element_x0 <= 'd0;
 end
@@ -648,7 +752,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_y0 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d0) 
-    accu_element_y0 <= accu_element_y0 + total_element[7:0];    
+    accu_element_y0 <= accu_element_wirey0;    
   else if (current_state == ST_CHECK)
     accu_element_y0 <= 'd0;
 end
@@ -658,7 +762,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_x1 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d1) 
-    accu_element_x1 <= accu_element_x1 + total_element[15:8];    
+    accu_element_x1 <= accu_element_wirex1;    
   else if (current_state == ST_CHECK)
     accu_element_x1 <= 'd0;
 end
@@ -666,7 +770,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_y1 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d1) 
-    accu_element_y1 <= accu_element_y1 + total_element[7:0];    
+    accu_element_y1 <= accu_element_wirey1;    
   else if (current_state == ST_CHECK)
     accu_element_y1 <= 'd0;
 end
@@ -675,7 +779,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_x2 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d2) 
-    accu_element_x2 <= accu_element_x2 + total_element[15:8];    
+    accu_element_x2 <= accu_element_wirex2;    
   else if (current_state == ST_CHECK)
     accu_element_x2 <= 'd0;
 end
@@ -683,7 +787,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_y2 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d2) 
-    accu_element_y2 <= accu_element_y2 + total_element[7:0];    
+    accu_element_y2 <= accu_element_wirey2;    
   else if (current_state == ST_CHECK)
     accu_element_y2 <= 'd0;
 end
@@ -692,7 +796,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_x3 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d3) 
-    accu_element_x3 <= accu_element_x3 + total_element[15:8];    
+    accu_element_x3 <= accu_element_wirex3;    
   else if (current_state == ST_CHECK)
     accu_element_x3 <= 'd0;
 end
@@ -700,7 +804,7 @@ always @(posedge clk) begin
   if (!rst_n)
     accu_element_y3 <= 'd0;    
   else if (current_state==ST_GROUP_ACC && group_acc_clus_count=='d4 && min_idx=='d3) 
-    accu_element_y3 <= accu_element_y3 + total_element[7:0];    
+    accu_element_y3 <= accu_element_wirey3;    
   else if (current_state == ST_CHECK)
     accu_element_y3 <= 'd0;
 end
